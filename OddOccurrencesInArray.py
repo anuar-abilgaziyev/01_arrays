b81e129c4888143517a235720287c78b6608e693
Anuar Abilgaziyev <73526427+anuar-abilgaziyev@users.noreply.github.com>
2020-12-12 22:26:12 +0600

    Update OddOccurrencesInArray.py

--- OddOccurrencesInArray.py
+++ OddOccurrencesInArray.py
@@ -1,5 +1,12 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
+'''
+A non-empty array A consisting of N integers is given. 
+The array contains an odd number of elements, 
+and each element of the array can be paired with 
+another element that has the same value, 
+except for one element that is left unpaired.
+
+The function, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+'''
 
 def solution(A):
     freq = {} 
