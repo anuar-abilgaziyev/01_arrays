e7ae4d4a46b385e2d262c5f8d26a9b007050fbb5
Anuar Abilgaziyev <73526427+anuar-abilgaziyev@users.noreply.github.com>
2020-12-12 18:46:16 +0600

    Update CyclicRotation.py

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -1,5 +1,10 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
+'''
+Rotation of the array means that each element is shifted right by one index, 
+and the last element of the array is moved to the first place. 
+
+For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] 
+(elements are shifted right by one index and 6 is moved to the first place).
+'''
 
 def solution(A, K):
     size = len(A)
